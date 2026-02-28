81357e01dc46631a3c9b22968c5277d7a59e32e1
alb91 <alb9112@gmail.com>
2026-02-28 13:07:40 -0600

    removed test

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,6 +1,5 @@
 <?php 
-echo "SLIM IS RUNNING";
-exit;
+
 require __DIR__ . '/../vendor/autoload.php'; 
 use Psr\Http\Message\ResponseInterface as Response; 
 use Psr\Http\Message\ServerRequestInterface as Request; 
